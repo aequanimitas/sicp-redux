c9d56fe2cff14467dab8309a28bf3de9b35f512a
metalfatigue <htavenido@gmail.com>
2014-07-14 13:48:13 +0800

    inefficient but fun

--- chapter_1/1.1.4_Compound_Procedures/sum_of_squares.scm
+++ chapter_1/1.1.4_Compound_Procedures/sum_of_squares.scm
@@ -1,3 +1,9 @@
 (load "square.scm")
 (define (sum-of-squares x y) (+ (square x) (square y)))
 (sum-of-squares 7 8)
+
+(define (sum-of-squares-lambda x y)
+    (+ ((lambda (a) (* a a)) x)
+       ((lambda (b) (* b b)) y)))
+
+(sum-of-squares-lambda 7 8)

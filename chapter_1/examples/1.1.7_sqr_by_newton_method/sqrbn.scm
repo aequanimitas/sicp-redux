27e1b270f38e6529528e2be1cdd041cfc0f5b0cb
metalfatigue <htavenido@gmail.com>
2014-07-28 09:52:13 +0800

    slow, but almost clear analysis of word problem

---
+++ chapter_1/examples/1.1.7_sqr_by_newton_method/sqrbn.scm
@@ -0,0 +1,19 @@
+(define (better-guess guess x)
+  (/ (+ guess (/ x guess)) 2))
+
+; assume that 1 is the square root of all numbers'
+; using decimal here (1.0) ensures us that all rational numbers yield to decimal ones
+; though I know there's a lib to enforce evaluation in decimal
+
+(define (start x)
+  (sqrtn 1.0 x))
+
+(define (good-enough? guess radicand)
+  (< (abs (- (* guess guess) radicand)) 0.001))
+
+(define (sqrtn guess radicand)
+  (if (good-enough? guess radicand)
+    guess
+    (sqrtn (better-guess guess radicand) radicand)))
+
+(start 9)

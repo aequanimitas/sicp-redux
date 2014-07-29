56977dd0e5570fa19f7db729d72d3f6a5858aea2
metalfatigue <htavenido@gmail.com>
2014-07-29 14:25:01 +0800

    working newton sqrt finding proc, struggling to finalize 1.7

--- chapter_1/exercises/1.7/newton_method_root.scm
+++ chapter_1/exercises/1.7/newton_method_root.scm
@@ -6,4 +6,13 @@
   ; y is the guess
   (average y (/ x y)))
 
-(better-approximation 9 3)
+(define (newton-sqrt-find x guess)
+  (if (approximation-threshold? x guess)
+      guess
+      (newton-sqrt-find x (better-approximation x guess))))
+      
+(define (approximation-threshold? x guess)
+  ; 0.001 is our threshold
+  (< (abs (- (* guess guess) x)) 0.001))
+
+(newton-sqrt-find 0.001 1.0)

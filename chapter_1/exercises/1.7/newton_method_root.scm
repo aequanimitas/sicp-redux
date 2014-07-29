9f54c2803b57cea5e2e5e11e99e6fa2f566023f9
metalfatigue <htavenido@gmail.com>
2014-07-29 14:28:50 +0800

    finally 1.7

--- chapter_1/exercises/1.7/newton_method_root.scm
+++ chapter_1/exercises/1.7/newton_method_root.scm
@@ -6,13 +6,14 @@
   ; y is the guess
   (average y (/ x y)))
 
-(define (newton-sqrt-find x guess)
-  (if (approximation-threshold? x guess)
+(define (newton-sqrt-find x guess previous-guess)
+  (if (approximation-threshold? guess previous-guess)
       guess
-      (newton-sqrt-find x (better-approximation x guess))))
+      (newton-sqrt-find x (better-approximation x guess) guess)))
       
-(define (approximation-threshold? x guess)
+(define (approximation-threshold? guess previous-guess)
   ; 0.001 is our threshold
-  (< (abs (- (* guess guess) x)) 0.001))
+  ; 'watch guess in each iteration until the threshold is so small'
+  (< (abs (- guess previous-guess)) 0.001))
 
-(newton-sqrt-find 0.001 1.0)
+(newton-sqrt-find 0.001 1.0 0.0)

a4c1711edff403c0e3aee8f5201de7c2e441155c
metalfatigue <htavenido@gmail.com>
2014-08-06 04:47:35 +0800

    add denomination category conditional

--- chapter_1/examples/1.2.2_Counting_Change/ph-coin.scm
+++ chapter_1/examples/1.2.2_Counting_Change/ph-coin.scm
@@ -1,1 +1,7 @@
+(define (peso-coin-denomination denom-key)
+  (cond ((= denom-key 1) 1)
+        ((= denom-key 2) 5)
+        ((= denom-key 3) 10)
+        ((= denom-key 4) 25)))
 
+(peso-coin-denomination 3)

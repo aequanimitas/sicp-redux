ee0f15a85b3751480ee59065a151cb128277e822
metalfatigue <htavenido@gmail.com>
2014-08-06 04:07:32 +0800

    fixed iterative fib eval

--- chapter_1/1.2.2_Tree_Recursion/fib.scm
+++ chapter_1/1.2.2_Tree_Recursion/fib.scm
@@ -1,7 +1,16 @@
-(define (fib n)
+(define (fib-r n)
   (cond ((= n 0) 0)
         ((= n 1) 1)
-        (else (+ (fib (- n 1))
-                 (fib (- n 2))))))
+        (else (+ (fib-r (- n 1))
+                 (fib-r (- n 2))))))
 
-(fib 9)
+(fib-r 9)
+
+(define (fib-i n)
+  (define (iter x y z)
+    (if (= z 0)
+        y
+        (iter (+ x y) x (- z 1))))
+  (iter 1 0 n))
+
+(= (fib-r 9) (fib-i 9))

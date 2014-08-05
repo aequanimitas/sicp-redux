969b6d29ba17e31d8e0f9681d859731ddfd31dea
metalfatigue <htavenido@gmail.com>
2014-08-06 05:04:36 +0800

    fixed count-change, localized cases

--- chapter_1/examples/1.2.2_Counting_Change/ph-coin.scm
+++ chapter_1/examples/1.2.2_Counting_Change/ph-coin.scm
@@ -4,4 +4,14 @@
         ((= denom-key 3) 10)
         ((= denom-key 4) 25)))
 
+(define (counting-change x)
+  (define (change x y)
+    (cond ((= x 0) 1)
+          ((or (< x 0) (= y 0)) 0)
+          (else (+ (change x (- y 1))
+                   (change (- x (peso-coin-denomination y)) y)))))
+  (change x 4))
+ 
+
 (peso-coin-denomination 3)
+(counting-change 100)

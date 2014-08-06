50444b91642c64fb6b41fec202ce1a062333b9ae
metalfatigue <htavenido@gmail.com>
2014-08-06 13:34:53 +0800

    this was easier to solve in pen and paper rather than in code

--- chapter_1/exercises/1.11/1.11.scm
+++ chapter_1/exercises/1.11/1.11.scm
@@ -1,9 +1,19 @@
-(define (f-recursive n)
+(define (f-r n)
   (if (< n 3)
       n
-      (+ (f-recursive (- n 1))
-         (* 2 (f-recursive (- n 2)))
-         (* 3 (f-recursive (- n 3))))))
+      (+ (f-r (- n 1))
+         (* 2 (f-r (- n 2)))
+         (* 3 (f-r (- n 3))))))
 
-(trace f-recursive)
-(f-recursive 3)
+(f-r 3)
+
+(define (f-i n)
+  (define (iterator w x y z)
+    (if (< z 3)
+        w
+        (iterator (+ w (* 2 x) (* 3 y)) x y (- z 1))))
+  (if (> 3 n)
+      n
+     (iterator 2 1 0 n)))
+
+(f-i 3)

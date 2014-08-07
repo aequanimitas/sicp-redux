70ed1f80a3a2ba8dbd86da67daa20edce2e02b0d
metalfatigue <htavenido@gmail.com>
2014-08-07 19:13:39 +0800

    added a slightly different way on iteration

--- chapter_1/1.2.2_Tree_Recursion/fib.scm
+++ chapter_1/1.2.2_Tree_Recursion/fib.scm
@@ -6,11 +6,19 @@
 
 (fib-r 9)
 
-(define (fib-i n)
+(define (fib-i-01 n)
   (define (iter x y z)
     (if (= z 0)
         y
         (iter (+ x y) x (- z 1))))
   (iter 1 0 n))
 
-(= (fib-r 9) (fib-i 9))
+(define (fib-i-02 n)
+  (define (iter x y z)
+    (if (= z n)
+        y
+        (iter (+ x y) x (+ z 1))))
+                                    
+  (iter 1 0 0))
+
+(= (fib-r 9) (fib-i-02 9) (fib-i-01 9))

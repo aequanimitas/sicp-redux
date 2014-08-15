99619c19786e18bb902071fd774f4fe8d71c81b1
metalfatigue <htavenido@gmail.com>
2014-08-15 09:38:01 +0800

    fixed, mind twisted, again

--- chapter_1/1.2.4_Exponentiation/faster_exponent_iteration.lisp
+++ chapter_1/1.2.4_Exponentiation/faster_exponent_iteration.lisp
@@ -10,11 +10,11 @@
     (format t "Exponent: ~A~%" exp)
     (format t "~%"))
 
-  (defun itr (exp acc)
+  (defun itr (exp acc base)
     (logger exp acc)
-    (cond ((= exp 1) acc)
-          ((even? exp) (itr (/ exp 2) (* acc (square base))))
-          ((odd? exp) (itr (- exp 1) (* acc base)))))
-  (itr exponent 1))
+    (cond ((= exp 0) acc)
+          ((even? exp) (itr (/ exp 2) acc (square base)))
+          ((odd? exp) (itr (- exp 1) (* acc base) base))))
+  (itr exponent 1 base))
 
-(format t "~A" (fei 2 20))
+(format t "~A" (fei 2 10))

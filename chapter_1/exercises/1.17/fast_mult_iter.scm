8186d8db1de5831cf8bd0f48fc03e0fe449210ad
metalfatigue <htavenido@gmail.com>
2014-08-17 04:38:33 +0800

    multiplication logarithmic time second attempt

--- chapter_1/exercises/1.17/fast_mult_iter.scm
+++ chapter_1/exercises/1.17/fast_mult_iter.scm
@@ -1,15 +1,33 @@
 (define (* a b)
   (define (halve x)
     (/ x 2))
+
   (define (dub x)
     (+ x x))
-  (define (iter x y accumulator)
-    (cond ((= y 0) 0)
-          ((= y 1) x)
-          (else iter x (- y 1) (+ accumulator (dub x)))))
-  (iter a b 0))
 
-(* 2 0)
-(* 2 1)
-(* 2 2)
-(* 2 3)
+  (define (is-even? x)
+    (= (remainder (/ x 2)) 0))
+
+  (define (is-odd? x)
+    (not (is-even? x)))
+  (define (iter y accumulator eval-count)
+    (cond ((= y 0)
+            (newline)
+            (display "Evaluation counter: ")
+            (display eval-count)
+            accumulator)
+          (is-odd? (iter (- y 1) (+ accumulator a) (+ eval-count 1)))
+          (is-even? (iter (halve y) (dub a) (+ eval-count 1)))))
+
+  (iter b 0 0))
+
+(define (generate-multiples x y)
+  (define (iter w state)
+    (newline)
+    (display state)
+    (if (> w y)
+        state
+        (iter (+ w 1) (* x w))))
+  (iter 0 0))
+
+(* 2 8)

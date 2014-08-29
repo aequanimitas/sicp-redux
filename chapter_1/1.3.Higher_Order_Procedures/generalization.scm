581b671ead74c84bc4bc928baae8f338c6c4dc0d
metalfatigue <htavenido@gmail.com>
2014-08-30 04:47:53 +0800

    A more general case function for combining numbers in series
    - solved one problem

--- chapter_1/1.3.Higher_Order_Procedures/generalization.scm
+++ chapter_1/1.3.Higher_Order_Procedures/generalization.scm
@@ -12,13 +12,24 @@
         (iter (+ a 1) (- b 1) (* (termfn a) result)))) 
   (iter x y 1))
 
+(define (accumulate x y termfn combiner start-value)
+  (define (iter a b result)
+    (if (= b 0)
+        result
+        (iter (+ a 1) (- b 1) (combiner (termfn a) result)))) 
+  (iter x y start-value))
+
+(define (sum-range-a x y termfn)
+  (accumulate x y termfn + 0))
+
+(define (product-range-a x y termfn)
+  (accumulate x y termfn * 1))
+
 (define (identity x)
   x)
 
 (define (cube x)
   (* x x x))
 
-(sum-range 1 10 identity)
-(sum-range 1 10 cube)
-(product-range 10 1 identity)
-(product-range 10 1 cube)
+(= (sum-range 1 10 identity) (sum-range-a 1 10 identity))
+(= (product-range 1 10 identity) (product-range-a 1 10 identity))

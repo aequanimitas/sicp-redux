5a32ac3c4396fe4bd3a2ff58132dc2d9685ee3ec
metalfatigue <htavenido@gmail.com>
2014-08-29 09:09:25 +0800

    argument cleanup

--- chapter_1/1.3.Higher_Order_Procedures/generalization.scm
+++ chapter_1/1.3.Higher_Order_Procedures/generalization.scm
@@ -8,11 +8,11 @@
   (= x 0))
 
 (define (accumulate a b accu step combiner null-value c-term)
-  (define (iter a b accu step combiner null-value c-term)
+  (define (iter a b accu)
     (if (null-value b)
         accu
-        (iter a (step b) (combiner accu c-term) step combiner null-value c-term)))
-  (iter a b accu dec combiner nothing? c-term))
+        (iter a (step b) (combiner accu c-term))))
+  (iter a b accu))
 
 (define (sum a b)
   (accumulate a b a dec + nothing? 1))
@@ -20,6 +20,14 @@
 (define (product a b)
   (accumulate a (- b 1) a dec + nothing? a))
 
+; specific cases
+(define (sum-cubes a b)
+  (define (iter a b accu)
+    (if (= b 0)
+        accu
+        (iter (+ a 1) (- b 1) (+ accu (* a a a)))))
+  (iter a b 0))
+
 (sum 12 8)
 (product 4 5)
-(= (sum 12 8) (product 4 5)) 
+;(= (sum 12 8) (product 4 5)) 

a0a46112f6cca54fc57386f6185caf4064ea7d4b
metalfatigue <htavenido@gmail.com>
2014-08-31 11:43:23 +0800

    changed repeating lambdas back into functions, does not make sense

--- chapter_1/1.3.Higher_Order_Procedures/lambdas/1.3.1.scm
+++ chapter_1/1.3.Higher_Order_Procedures/lambdas/1.3.1.scm
@@ -32,13 +32,7 @@
 (define (cube x)
   (* x x x))
 
-(= (sum-range-i 2 3 (lambda (x) x) increment) (sum-range-r 2 3 identity increment))
-; (= (sum-range-i 2 3 square increment) (sum-range-r 2 3 square increment))
-; (= (sum-range-i 2 3 cube increment) (sum-range-r 2 3 cube increment))
-; (= (product-range-i 2 3 cube increment) (product-range-i 2 3 cube increment))
-; (= (product-range-i 2 3 square increment) (product-range-i 2 3 square increment))
-; (= (product-range-i 2 3 identity increment) (product-range-i 2 3 identity increment))
-
+(= (sum-range-i 2 3 identity increment) (sum-range-r 2 3 identity increment))
 
 ; exercise 1.32
 (define (accumulate-r x y term next combiner null-value)
@@ -52,21 +46,6 @@
       result
       (iter (next a) (combiner (term a) result))))
   (iter x null-value))
-
-(= (sum-range-i 2 3 identity increment) (accumulate-i 2 3 identity increment + 0))
-; (= (sum-range-i 2 3 square increment) (accumulate-i 2 3 square increment + 0))
-; (= (sum-range-i 2 3 cube increment) (accumulate-i 2 3 cube increment + 0))
-; (= (product-range-i 2 3 cube increment) (accumulate-i 2 3 cube increment * 1))
-; (= (product-range-i 2 3 square increment) (accumulate-i 2 3 square increment * 1))
-; (= (product-range-i 2 3 identity increment) (accumulate-i 2 3 identity increment * 1))
-
-
-(= (sum-range-r 2 3 identity increment) (accumulate-r 2 3 identity increment + 0))
-; (= (sum-range-r 2 3 square increment) (accumulate-r 2 3 square increment + 0))
-; (= (sum-range-r 2 3 cube increment) (accumulate-r 2 3 cube increment + 0))
-; (= (product-range-i 2 3 cube increment) (accumulate-r 2 3 cube increment * 1))
-; (= (product-range-i 2 3 square increment) (accumulate-r 2 3 square increment * 1))
-; (= (product-range-i 2 3 identity increment) (accumulate-r 2 3 identity increment * 1))
 
 (define (filtered-accumulate-i x y term next combiner null-value filter-cond)
   (define (iter a result)
@@ -90,14 +69,12 @@
 (define (primes-sum-square-range x y)
   (filtered-accumulate-i x y square increment + 0 prime?))
 
-(primes-sum-square-range 6 7)
+;(primes-sum-square-range 6 7)
 
 (define (are-coprimes? x y)
-  (define (co-prime? x)
-    (= 1 (gcd x y)))
   (define (fn z fun)
     (fun z))
-  (fn x co-prime?))
+  (fn x (lambda (x) (= 1 (gcd x y)))))
 
 (are-coprimes? 6 36)
 (are-coprimes? 5 36)
@@ -110,13 +87,11 @@
     (= 1 (gcd x y)))
   (filtered-accumulate-i x y identity increment * 1 is-a-co-prime?))
 
-(product-of-coprimes-fn-scope 2 3)
+;(product-of-coprimes-fn-scope 2 3)
 
 (define (product-of-coprimes x y)
   (define (co-prime? x)
     (= 1 (gcd x y)))
   (filtered-accumulate-i x y identity increment * 1 co-prime?))
 
-(product-of-coprimes 2 3)
-
-(= (sum-range-i 2 3 (lambda (x) x) increment) (sum-range-r 2 3 identity increment))
+;(product-of-coprimes 2 3)

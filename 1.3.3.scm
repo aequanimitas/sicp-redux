bddd4f0913df603e90a54734e63efa1b740603cb
metalfatigue <htavenido@gmail.com>
2014-09-02 07:58:48 +0800

    add a thunk, let's see if this will make sense

--- 1.3.3.scm
+++ 1.3.3.scm
@@ -32,3 +32,19 @@
 (half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0)
 
 (half-interval-method sin 2.1 0.1)
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+         (if (close-enough? guess next)
+             next
+             (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+((lambda (x y) (< (abs (- x y)) ((lambda () 0.00001)))) 0 1) 

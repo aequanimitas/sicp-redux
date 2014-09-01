1b16654538d86a2134d1c28cd519e261524c7559
metalfatigue <htavenido@gmail.com>
2014-09-02 07:24:18 +0800

    simplify structure

---
+++ 1.3.3.scm
@@ -0,0 +1,34 @@
+; half interval method
+; similar to DAC
+
+; make this more flexible with the number of arguments it can handle
+; nearer to actual average definition
+
+(define (average x y)
+  (/ (+ x y) 2))
+ 
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (search f neg-point pos-point)
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+           (cond ((positive? test-value) (search f neg-point midpoint))
+                 ((negative? test-value) (search f midpoint neg-point))
+                 (else midpoint))))))
+
+(search sin 4.0 2.0)
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+        (cond ((and (negative? a-value) (positive? b-value)) (search f a b))
+              ((and (negative? b-value) (positive? a-value)) (search f b a))
+              (else error "Values are not opposite sign: " a b))))
+
+(half-interval-method sin 2.0 4.0)
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3)) 1.0 2.0)
+
+(half-interval-method sin 2.1 0.1)

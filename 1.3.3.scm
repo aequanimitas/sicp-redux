2c46c34d8656e1f5b15aef456d9440d5ecdc2e53
metalfatigue <htavenido@gmail.com>
2014-09-02 08:40:34 +0800

    All signs are leading to the y-combinator, deferring this for now

--- 1.3.3.scm
+++ 1.3.3.scm
@@ -47,4 +47,26 @@
 
 (fixed-point cos 1.0)
 
-((lambda (x y) (< (abs (- x y)) ((lambda () 0.00001)))) 0 1) 
+; close-enough procedure        ; tolerance constant
+((lambda (x y) (< (abs (- x y)) ((lambda () 0.00001)))) 0 1)
+
+; sum of "fn" of two numbers
+((lambda (x y fn) (+ (fn x) (fn y))) 0.3 0.43 square)
+
+; converting this to its most basic form
+(define (sqrt x)
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (good-enough? guess previous-guess)
+    (< (abs (- guess previous-guess)) 0.001))
+  (define (newton-sqrt-find guess previous-guess)
+    (if (good-enough? guess previous-guess)
+        guess
+        (newton-sqrt-find (improve guess) guess)))
+  (newton-sqrt-find 1.0 0))
+
+(sqrt 0.001)
+    
+((lambda (x) ((lambda (x y) (/ (+ x y) 2.0)) x (/ x x))) 1.0)

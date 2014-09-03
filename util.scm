95d5de0f685efa67642965f90a28522f2b7ff7f0
metalfatigue <htavenido@gmail.com>
2014-09-03 13:50:59 +0800

    no changes here, just commit

--- util.scm
+++ util.scm
@@ -1,3 +1,16 @@
 ; naive implementation of average, but limiting myself for now in just using 2 arguments
 (define (average x y)
   (/ (+ x y) 2))
+
+(define (fixed-point f first-guess)
+  (define tolerance 0.0001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (newline)
+    (display guess)
+    (let ((next (f guess)))
+         (if (close-enough? guess next)
+             next
+             (try next))))
+  (try first-guess))

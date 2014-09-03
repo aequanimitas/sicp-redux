6fdac4d5af7548522d85a7316a7a05b6d07585db
metalfatigue <htavenido@gmail.com>
2014-09-03 12:32:01 +0800

    Exercise 1.41
    damn, that's cool

--- 1.3.4.scm
+++ 1.3.4.scm
@@ -3,3 +3,17 @@
   (lambda (x) (average x (f x))))
 
 ((average-damp square) 10)
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (average y (/ x (square y))))) 1.0))
+
+(cube-root 125)
+
+; exercise 1.41
+(define (double fn)
+  (lambda (x) (fn (fn x))))
+
+(define (inc x)
+  (+ 1 x))
+
+(((double (double double)) inc) 5)

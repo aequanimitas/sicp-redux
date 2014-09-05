269e6a8012264cbbde483fe8623c7923fed746b0
metalfatigue <htavenido@gmail.com>
2014-09-06 05:44:31 +0800

    exercise 2.2

--- 2.scm
+++ 2.scm
@@ -40,3 +40,41 @@
 (mr -1 -3)
 (mr 1 -3)
 (mr -1 3)
+
+
+; exercise 2.2
+
+(define (make-point x y)
+  (cons x y))
+
+(make-point 3 -2)
+
+(define (make-segment x y)
+  (cons x y))
+
+(define (x-point x)
+  (car x))
+
+(define (y-point x)
+  (car x))
+
+(define (start-segment x)
+  (car x))
+
+(define (end-segment x)
+  (cons x))
+
+(define ex2.2.01 (make-segment (make-point -3 19) (make-point -4 27)))
+
+(start-segment ex2.2.01)
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")")
+  0)
+
+(print-point (make-point -3 19))

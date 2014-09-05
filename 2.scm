744e80f347fdd70079418e49ba6042a7b28e111f
metalfatigue <htavenido@gmail.com>
2014-09-05 11:20:35 +0800

    Early stages of chapter 2

---
+++ 2.scm
@@ -0,0 +1,31 @@
+(define (linear-combination a b x y)
+  (+ (* a x) (* b y)))
+  
+; 2.1 Intro
+
+(define (make-rational-basic n d) (cons n d))
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(make-rational-basic 1 3)
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  x)
+
+(print-rat (make-rational-basic 1 3))
+
+(define (make-rational-lowest-terms n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(print-rat (make-rational-lowest-terms 20 4))
+
+(define (add-rat x y)
+  (make-rational-lowest-terms (+ (* (numer x) (denom y)) (* (denom x) (numer y))) (* (denom x) (denom y))))
+
+(print-rat (add-rat (make-rational-lowest-terms 1 3) (make-rational-lowest-terms 1 3)))
+
+(print-rat (make-rational-basic -10 99))

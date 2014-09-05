ef63921865060c35bde720a47f55f0c8c740453e
metalfatigue <htavenido@gmail.com>
2014-09-06 04:06:52 +0800

    exercise 2.1 done

--- 2.scm
+++ 2.scm
@@ -32,9 +32,11 @@
 
 ; Exercise 2.1
 (define (mr n d)
-  (if (> 0 d)
-      (mr (* -1 n) (* -1 d))
-      (let ((g (gcd n d)))
-           (cons (/ n g) (/ d g))))) 
+  (cond ((and (> n 0) (< d 0)) (mr (* -1 n) (* -1 d)))
+        ((and (< d 0) (< n 0)) (mr (* -1 n) (* -1 d)))
+        (else (let ((g (gcd n d)))
+              (cons (/ n g) (/ d g))))))
    
 (mr -1 -3)
+(mr 1 -3)
+(mr -1 3)

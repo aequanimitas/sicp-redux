fdb2a9607b18228cacc16eea82cddcb400c5eb04
metalfatigue <htavenido@gmail.com>
2014-09-24 13:07:12 +0800

    reviewing previous material

--- 1.js
+++ 1.js
@@ -1,3 +1,13 @@
+console.log(22);
+console.log("p");
+
+try {
+  console.log(p);
+} catch (e) {
+  console.log(e);
+}
+
+// exercise 1.31 - Higher Order Functions
 function sum_range (x, y, term, next) {
   function iter (a, result) {
      return a > y ? result : iter( next(a), result + term(a));
@@ -9,8 +19,8 @@ function identity(x) {
   return x;
 }
 
-function increment(x) {
+function increment1(x) {
   return x + 1;
 }
 
-console.log(sum_range(1, 10, identity, increment));
+console.log(sum_range(1, 10, identity, increment1));

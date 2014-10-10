0f36ae4bc51621192c6288d5888eb9fee6f2507c
metalfatigue <htavenido@gmail.com>
2014-10-11 00:10:28 +0800

    working but I don't like it

--- list-operators.scm
+++ list-operators.scm
@@ -1,19 +1,29 @@
-(define (repeater at-limit? object process next step accumulator)
-  (if (at-limit? object)
-      accumulator 
-      (repeater at-limit? 
+(define (repeater done? object process next step accumulator refine)
+  (if (done? object)
+      (refine accumulator)
+      (repeater done? 
                 (process object) 
                 process 
                 next 
                 step 
-                (next (step object) accumulator))))
-
+                (next (step object) accumulator)
+                refine)))
+(define (identity x) x)
 (define (len l)
-  (repeater null? l (lambda (x) (cdr x)) + (lambda (x) 1)  0))
+  (repeater null? l cdr + (lambda (x) 1)  0 identity))
 
 (len (list 1 2 3 4))
 
 (define (rev l)
-  (repeater null? l (lambda (x) (cdr x)) cons (lambda (x) (car x)) (list)))
+  (repeater null? l cdr cons car (list) identity))
 
 (rev (list 1 2 3 4))
+
+(define (nth l i)
+  (define (iter x z)
+    (if (= z i)
+        (car x)
+        (iter (cdr x) (- z 1))))
+  (iter l (- (len l) 1)))
+
+(nth (list 1 2 3 4) 3)

8498759aab0af336d15a775b3c5fab4c3ceaf4eb
metalfatigue <htavenido@gmail.com>
2015-01-30 01:15:17 +0800

    handling edge cases

--- range.scm
+++ range.scm
@@ -1,13 +1,22 @@
 (define (range start end . step)
-  (define (iter start end accum)
+
+  (define (step-down step)
+    (< (car step) 0))
+        
+  (define (iter start end accum oper)
     (if (= end 0)
         (reverse accum)
-        (iter (+ (if (not (eq? step ()))
+        (iter (oper (if (not (eq? step ()))
                        (car step)
-                       1) start) (- end 1) (cons start accum))))
-  (if (> start end)
-      ()
-      (iter start end ())))
+                       1) start) 
+              (- end 1) 
+              (cons start accum)
+              oper)))
+
+  (cond ((and (> start end) (step-down step)) (iter start end () -))
+        ((and (> start end) (not (step-down step))) ())
+        ((and (< start end) (step-down step)) ())
+        (else (iter start end () +))))
 
 (range 1 10 20)
 (range 1100 10 20)

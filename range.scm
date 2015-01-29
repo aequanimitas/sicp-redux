eef14698d78f9c336cb1e8d423e6e370898529fa
metalfatigue <htavenido@gmail.com>
2015-01-30 01:02:31 +0800

    improved range

--- range.scm
+++ range.scm
@@ -1,9 +1,14 @@
-(define (range start end)
+(define (range start end . step)
   (define (iter start end accum)
     (if (= end 0)
         (reverse accum)
-        (iter (+ 1 start) (- end 1) (cons start accum))))
-  (iter start end ()))
+        (iter (+ (if (not (eq? step ()))
+                       (car step)
+                       1) start) (- end 1) (cons start accum))))
+  (if (> start end)
+      ()
+      (iter start end ())))
 
-(range 1 10)
+(range 1 10 20)
+(range 1100 10 20)
 
